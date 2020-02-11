a3629e3f6a945fcac768f9188db7ec550ef04250
Omar <omarcr.96@gmail.com>
2020-02-11 12:53:11 -0600

    Diseño de la seccion Compradores y vendedores terminado

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // VARIABLES
 const contenedor = document.querySelector("#container");
-const form = document.getElementById('form')
-const compradores = document.getElementById('compradores')
+const form = document.getElementById('form');
+const compradores = document.getElementById('compradores');
+const vendedores = document.getElementById('vendedores');
+const formVendedor = document.getElementById('formVendedor');
 
 // TAMAÑO DE PANTALLA
 const comprobarAncho = () => {
@@ -17,16 +19,25 @@ window.addEventListener("resize", () => {
 });
 
 // TOGGLE DE BOTONES
+
+// menu lateral
 document.querySelector("#btn-menu").addEventListener("click", () => {
   contenedor.classList.toggle("active");
 });
-
+// formulario compradores
 document.querySelector("#btn-add").addEventListener("click", () => {
   form.classList.toggle("form-active");
 });
-
+// menu compradores
 document.querySelector('#btn-compradores').addEventListener("click", () => {
   compradores.classList.toggle("compradores-active")
 })
-
+// menu vendedores
+document.querySelector('#btn-vendedores').addEventListener('click', () => {
+  vendedores.classList.toggle("vendedores-active")
+})
+// formaulario vendedores
+document.querySelector('#btn-addVendedores').addEventListener('click', () => {
+  formVendedor.classList.toggle("formVendedor-active")
+})
 
